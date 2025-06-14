8faa1ceda5769ae133bb9e68bca1c535e8efe246
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-06-14 14:27:49 +0800

    fix permission middleware to handle populated role

--- server/src/middleware/permission.js
+++ server/src/middleware/permission.js
@@ -1,7 +1,13 @@
 import Role from '../models/role.model.js'
 
 export const requirePerm = (perm) => async (req, res, next) => {
-  const role = await Role.findById(req.user.roleId)
+  let role = req.user.roleId
+
+  // `protect` middleware may have populated roleId
+  if (!role?.permissions) {
+    role = await Role.findById(req.user.roleId)
+  }
+
   if (!role || !role.permissions.includes(perm)) {
     return res.status(403).json({ message: '權限不足' })
   }
