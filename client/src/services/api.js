4e4aac4f9372ef14669e691108745ffd5667c012
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-06-22 03:49:10 +0800

    api.js update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:3000/api',
+  withCredentials:true,
 })
 
 /* ---------- 請求攔截：帶上 JWT ---------- */
