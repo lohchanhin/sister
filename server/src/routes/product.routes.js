948e65932fc7018184633caf0a7e7b6a6896f4e2
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-08-23 16:25:48 +0800

    test: cover product signed url

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { protect } from '../middleware/auth.js'
 import { requirePerm } from '../middleware/permission.js'
 import { PERMISSIONS } from '../config/permissions.js'
-import { getProducts, getProduct, batchDownload, getBatchDownloadProgress } from '../controllers/product.controller.js'
+import { getProducts, getProduct, batchDownload, getBatchDownloadProgress, getProductSignedUrl } from '../controllers/product.controller.js'
 
 const router = Router()
 
@@ -10,5 +10,6 @@ router.post('/batch-download', protect, batchDownload)
 router.get('/batch-download/:id', protect, getBatchDownloadProgress)
 router.get('/', protect, requirePerm(PERMISSIONS.ASSET_READ), getProducts)
 router.get('/:id', protect, requirePerm(PERMISSIONS.ASSET_READ), getProduct)
+router.get('/:id/url', protect, requirePerm(PERMISSIONS.ASSET_READ), getProductSignedUrl)
 
 export default router
